e37a25b166573345f5bd4d099bbc2e6b5c5e4c87
BruceXuheng <BruceXuheng@163.com>
2020-08-01 22:30:32 +0800

    优化DrawerLayout用法

--- morecode/chen_materialdsign/src/main/java/com/chenxh/recyclerview/chen_materialdsign/demo/DrawerActivity.java
+++ morecode/chen_materialdsign/src/main/java/com/chenxh/recyclerview/chen_materialdsign/demo/DrawerActivity.java
@@ -3,8 +3,11 @@ package com.chenxh.recyclerview.chen_materialdsign.demo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -19,6 +22,32 @@ public class DrawerActivity extends AppCompatActivity {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        drawerLayout.openDrawer(Gravity.LEFT);
+
+        CountDownTimer timer = new CountDownTimer(10000,1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                drawerLayout.closeDrawer(Gravity.LEFT);
+
+            }
+        }.start();
+        NavigationView viewById = findViewById(R.id.nav_view);
+        viewById.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawer(GravityCompat.END);
+                return false;
+            }
+        });
+
 
     }
 }
